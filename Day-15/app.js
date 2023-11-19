ab72332d579f32f47d034356b19ae5087117d6c1
its0vivek <vivekmali108@gmail.com>
2023-11-19 10:05:02 +0530

    !st js

---
+++ Day-15/app.js
@@ -0,0 +1,83 @@
+// conditional Statements
+// console.log("before my if statement")
+// let age = 16;
+// if (age >= 18) {
+//  console.log("you can vote");
+//  console.log("you can drive");
+// }
+// if (age <= 18) {
+//     console.log("your not able to vote and drive")
+// }
+// console.log("after my if statement")
+///////////////////////////////////////////////////////////////////////////
+// let Months = 1;
+
+// switch(Months){
+//     case 1 : 
+//         console.log("January,February,March")
+//         break;
+//     case 2 :
+//         console.log("April,May,June")
+//         break;
+//     case 3 :
+//         console.log("October,Novenber,December")
+//         break;
+//     default :
+//         console.log("wronge months")
+
+//////////////////////////////////////////////////////////////////////////
+
+// let str = "apple"
+
+// if (str[0]==='a' || str[0]==='A' && str.length >= 5) {
+//     console.log("Gold string")
+// }else { console.log("Not a Gold string") }
+
+//////////////////////////////////////////////////////////////////////////
+ 
+// let num = 20;
+
+// if (num % 10 == 0) {
+//     console.log("good")
+// }else {
+//     console.log("bad"
+//     )
+// }
+
+// let Name = prompt("Enter the Name")
+// let age = prompt("Enter the age")
+
+// console.log(Name + " " + "is" + " " + age + " " + "years old")
+
+//////////////////////////////////////////////////////////////////////////
+
+// let  a = 311;
+// let  b = 142;
+// let  c = 53;
+
+// if ( a > b) {
+//     if (a > c) {
+//         console.log(a, "is largest")
+//     }else {
+//         console.log(c, "is largest")
+//     }
+// } else {
+//     if (b > c) {
+//         console.log(b, "is largest")
+//     }else {
+//         console.log(c, "is largest")
+//     }
+// }
+
+///////////////////////////////////////////////////////////////////////////
+
+let num1 = 32;
+let num2 = 311422;
+
+if ( (num1%10) == (num2%10) ) {
+    console.log("last digit is 2")
+}else {
+    console.log("last Digit is not 2")
+}
+
+////////////////////////////////////////////////////////////////////////////
