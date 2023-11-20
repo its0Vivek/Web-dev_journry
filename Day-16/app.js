e9c365d256fb074b4d171e287456a752fec31531
its0vivek <vivekmali108@gmail.com>
2023-11-20 20:48:58 +0530

    js

--- Day-16/app.js
+++ Day-16/app.js
@@ -16,26 +16,54 @@
 
 //////////////////////////////////////////////////////////////////////////
 
-let msg = "help!";
-newMsg = msg.trim().toUpperCase();
-console.log(newMsg)
+// let msg = "help!";
+// newMsg = msg.trim().toUpperCase();
+// console.log(newMsg)
  
 /////////////////////////////////////////////////////////////////
 
-let name = "ApnaCollege";
-let str = name.slice(4)
+// let name = "ApnaCollege";
+// let str = name.slice(4)
 
 //////////////////////////////////////////////////////////
 
-let vivek = ["vivek","niku","raju","vilas","niketan"];
+// let vivek = ["vivek","niku","raju","vilas","niketan"];
 
-///////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 
-let info = ["vivek",34,32]
+// let info = ["vivek",34,32]
 
-/////////////////////////////
-let newarr = [] //empty arrays
+// ////////////////////////////////////////////////////////////////////
+// let newarr = [] //empty arrays
 
-///////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 
+let arr = [7,8,9,-2];
+let n = 3;
 
+let ans = arr.slice(0,n);
+console.log(ans)
+
+let ans1 = arr.slice(0,-n);
+console.log(ans1)
+
+///////////////////////////////////////////////////////////////////////////
+
+// let pro = prompt("enter the string")
+
+// if(pro.length == 0) {
+//     console.log("string is empty")
+// }else {
+//     console.log("string is not empty")
+// }
+
+//////////////////////////////////////////////////
+
+let cha = "APnaCoLLage";
+let chh = 3;
+
+if (cha(chh) == cha.toLowerCase()) {
+    console.log("characater is empty")
+}else{
+    console.log("characater is not empty")
+}
