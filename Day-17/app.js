def63cc0c2b4a98b30c0bb6ac2ac4ac0dc806e4f
its0vivek <vivekmali108@gmail.com>
2023-11-22 19:52:43 +0530

    dune loop

--- Day-17/app.js
+++ Day-17/app.js
@@ -1,7 +1,43 @@
 // for loop
-for(let i=1; i<=5; i++) {
-    console.log(i);
-}
+// for(let i=1; i<=5; i++) {
+//     console.log(i);
+// }
 
 ////////////////////////////////////////////////////////////////
+//print odd no
 
+// for(let i=1; i<=15; i=i+2) {
+//     console.log(i);
+// }
+
+//////////////////////////////////////////
+//even number
+
+// for(let i=2; i<=15; i=i+2) {
+//     console.log(i);
+// }
+
+/////////////////////////////////////////////
+// print multiple of 5
+// for (let n=5; n<=50; n=n+5) {
+//     console.log(n);
+// }
+
+//////////////////////////////////////////////////
+/// make table
+
+// let n = prompt("enter your number")
+// n = parseInt(n)
+
+// for(let i=n; i<=n*10; i=i+n) {
+//     console.log(i);
+// }
+
+//////////////////////////////////////////////////////////
+//Nexted loop 
+
+for(let i=1; i<=3; i++) {
+    for(let j=1; j<=3; j++) {
+        console.log(j);
+    }
+}
